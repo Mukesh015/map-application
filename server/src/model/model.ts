4bbd2ab400847c121cc598b6f68e37aab1d2b6ea
Sudip Kundu <sudip2003kundu@gmail.com>
2024-04-08 03:04:35 +0530

    server

---
+++ server/src/model/model.ts
@@ -0,0 +1,40 @@
+import mongoose, { Document, Model } from "mongoose";
+
+interface UserDocument extends Document {
+    email: string;
+    username: string;
+    password: string;
+    avatar?: string;
+    history: any[]; // Adjust type according to your requirements
+    analytics: { date: Date; watchTime: number }[]; // Assuming analytics store datewise watchtime
+}
+
+const userSchema = new mongoose.Schema<UserDocument>(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        avatar: String,
+        history: [mongoose.Schema.Types.Mixed], // Adjust type according to your requirements
+        analytics: [
+            {
+                date: { type: Date,},
+                watchTime: { type: Number,}
+            }
+        ]
+    }
+);
+
+const UserModel: Model<UserDocument> = mongoose.model<UserDocument>("Users", userSchema);
+
+export { UserModel };
---
+++ server/src/router/route.ts
@@ -0,0 +1,5 @@
+import express from 'express';
+
+const UserRouter = express.Router();
+
+export default UserRouter;
