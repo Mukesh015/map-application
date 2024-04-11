3c633d12b3f31586452beee81d5c31d8127dce78
Mukesh Kr. Gupta <rg37090@gmail.com>
2024-04-11 14:01:37 +0530

    merged successfully

--- server/src/router/route.ts
+++
@@ -1,5 +0,0 @@
-import express from 'express';
-import Register  from '../controllers/user';
-const UserRouter = express.Router();
-UserRouter.post('/register', Register);
-export default UserRouter;
